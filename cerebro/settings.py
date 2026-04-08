8bc1c7cc75ab9a5731feb483b9c3bbb651705600
chrisim <bucekabirisimeonbirindwa@gmail.com>
2026-04-08 12:18:52 +0200

    Deplacement se sqlite3 dans data via les settings

--- cerebro/settings.py
+++ cerebro/settings.py
@@ -54,7 +54,7 @@ WSGI_APPLICATION = 'cerebro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR,'data/db.sqlite3'),
     }
 }
 
